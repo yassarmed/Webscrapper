0e19305d5b2ea8151399e9bcadb3a371ffd44a95
Yassar Ahmed <yassarahmed17@gmail.com>
2023-08-14 19:48:11 -0500

    init

---
+++ db/migrate/20230815004531_create_scraped_data.rb
@@ -0,0 +1,10 @@
+class CreateScrapedData < ActiveRecord::Migration[7.0]
+  def change
+    create_table :scraped_data do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
